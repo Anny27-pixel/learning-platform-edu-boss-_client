71e6ed1bb8ffc18a6f0c8bbd0a5b7138ace39b50
Anny27-pixel <jabintasmin102523@gmail.com>
2022-10-26 21:25:02 +0600

    load data from server side and setcategories

--- src/Pages/Shared/SideNav/SideNav.js
+++ src/Pages/Shared/SideNav/SideNav.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 const SideNav = () => {
+    const [courseCategories, setCourseCategories] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/courses-categories')
+            .then(res => res.json())
+            .then(data => setCourseCategories(data));
+    }, [])
     return (
         <div>
-            <h2>this is side nav</h2>
+            <h4>All Courses Category : {courseCategories.length}</h4>
         </div>
     );
 };
