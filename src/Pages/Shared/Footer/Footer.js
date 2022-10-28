b99eebfbb106656c934d1a8631c63c3185cd9d35
Anny27-pixel <jabintasmin102523@gmail.com>
2022-10-28 17:40:35 +0600

    footer added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
+import Card from 'react-bootstrap/Card';
 const Footer = () => {
     return (
-        <div>
-            <p>copyright coming soon!!!</p>
-        </div>
+        <Card className='bg-primary text-light'>
+            <Card.Header className='text-center'>Edu Boss</Card.Header>
+            <Card.Body>
+                <blockquote className="blockquote mb-0 text-center">
+                    <p>
+                        <small>copyright coming soon!!!</small>
+                    </p>
+                </blockquote>
+            </Card.Body>
+        </Card>
     );
 };
 
